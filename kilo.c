feeb971cba928fb3575c55ce70a472daccb7147f
dead2dog <dead2dog@outlook.com>
2022-11-18 20:41:06 +0800

    raw mode

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 #include<ctype.h>
+#include<errno.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -6,33 +7,48 @@
 
 struct termios orig_termios;
 
+void die(const char *s)//err handing
+{
+    perror(s);
+    exit(1);
+}
+
 void disbaleRawMode()//恢复shell
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");  
 }
 
 
 void enableRawMode() //原始模式
 {
-    tcgetattr(STDIN_FILENO,&orig_termios);
+    if(tcgetattr(STDIN_FILENO,&orig_termios) == -1) die("tcgetattr");
     atexit(disbaleRawMode); //注册终止函数
 
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(IXON|ICRNL);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO|ICANON|ISIG|IEXTEN);
-    tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw)) die("tcsetattr");
 }
 
 int main()
 {
     enableRawMode();
-    char c;
-    while(read(STDIN_FILENO,&c,1) == 1 && c != 'q')
+    while(1)
+    {
+        char c ='\0';
+        if(read(STDIN_FILENO,&c,1) == -1 && errno != EAGAIN) die("read");
         if(iscntrl(c))
         {
-            printf("%d\n",c);
+            printf("%d\r\n",c);
         }else{
-            printf("%d ('%c')\n",c,c);
+            printf("%d ('%c')\r\n",c,c);
         }
+        if(c == 'q') break;
+    }
     return 0;
 }
