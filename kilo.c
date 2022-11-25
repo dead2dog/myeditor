46c9c4e2d04e624c4e2c00c16e2dbc2e7a1bb8c5
dead2dog <dead2dog@outlook.com>
2022-11-25 23:35:08 +0800

    Window size, the hard way

--- kilo.c
+++ kilo.c
@@ -58,10 +58,14 @@ void enableRawMode() //原始模式
     if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw)) die("tcsetattr");
 }
 
-int getCursorPosition(int *rows, int *cols) {
+int getCursorPosition(int *rows, int *cols) //Windows size: hard way
+ {
     char buf[32];
     unsigned int i = 0;
-    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1; 
+    //n can query the terminal for status information
+    //argument of 6 to ask for the cursor position
+    //read the reply from the STDIN_FILENO
     while (i < sizeof(buf) - 1) {
         if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
         if (buf[i] == 'R') break;
@@ -70,13 +74,14 @@ int getCursorPosition(int *rows, int *cols) {
     buf[i] = '\0';
     if (buf[0] != '\x1b' || buf[1] != '[') return -1;
     if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+    //sscanf() can parse 24;80 and put the values into the rows and cols variables
     return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) { //eazy way err
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;   //游标移到右下角
         return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
@@ -98,6 +103,7 @@ void editorProcessKeypress() {
 
 /*** output ***/
 void editorDrawRows() {
+    //打印~
     int y;
     for (y = 0; y < E.screenrows; y++) {
         write(STDOUT_FILENO, "~\r\n", 3);
@@ -105,18 +111,19 @@ void editorDrawRows() {
 }
 
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4); 
+    write(STDOUT_FILENO, "\x1b[2J", 4);  //清楚屏幕
     //J -> erase in display ; 0j 1j 2j
-    //\x1 = \27 == [esc] ->escape character followed by [;
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    //\x1b = \27 == [esc] ->escape character followed by [;
+    write(STDOUT_FILENO, "\x1b[H", 3);   //移动浮标到0;0
     //H -> Reposition the cursor
-    editorDrawRows();
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    editorDrawRows();   //打印~
+    write(STDOUT_FILENO, "\x1b[H", 3);  
 }
 
 
 
 /*** init ***/
+
 void initEditor() {
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
